3198ab5db5efcea318339a820f6e99b050f222c2
Oreste NGABO <orestengabo3@gmail.com>
2024-06-03 15:15:04 +0200

    Styles on the improve and hero section

--- src/components/Home/Hero/Hero.tsx
+++ src/components/Home/Hero/Hero.tsx
@@ -5,7 +5,7 @@ const Hero = () => {
     <section className="d-flex p-5 bg-body-tertiary">
       <div className="text-button-area">
         <h1 className="mb-3">Elevate Your Photography</h1>
-        <p className="mb-5">
+        <p className="mb-5 text-secondary fs-5">
           Discover tips, techniques, and tutorials to help you become a better
           photographer.
         </p>
--- src/components/Home/Tips/Card.tsx
+++ src/components/Home/Tips/Card.tsx
@@ -14,7 +14,7 @@ const Card = ({image, description, headerCard, paragraph}: Props) => {
       <div className={`card`}>
           <img style={{width: '100%', height: 'auto', borderTopLeftRadius:'5px', borderTopRightRadius: '5px'}} src={image} alt={description}/>
           <h4 className="mt-3 text-center text-black">{headerCard}</h4>
-          <p className="p-3">{paragraph}</p>
+          <p className="p-3 text-secondary text-center">{paragraph}</p>
           <a href="#" className="btn btn-primary m-3">Learn More</a>
         </div>
     </div>
